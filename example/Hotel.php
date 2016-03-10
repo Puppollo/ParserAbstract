a4343125c4c367561a4c0c59b4cb14ee914a8a36
Anton V. Nazarov <a.nazarov@advmaker.net>
2016-03-10 16:02:10 +0300

    example fix

--- example/Hotel.php
+++ example/Hotel.php
@@ -18,6 +18,6 @@ class Hotel extends ParserAbstract
      */
     protected function element($element)
     {
-        file_put_contents($this->file, "{$element['Id']['value']}\n", FILE_APPEND);
+        file_put_contents($this->file, "{$element->Id}\n", FILE_APPEND);
     }
 }
